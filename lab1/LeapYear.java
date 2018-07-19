9043d7b18b005d4af25f542cca64136086c6d282
Juggernaut <macbookpro@Hurobo.local>
2018-07-19 11:41:19 +0800

    init

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -6,6 +6,15 @@ public class LeapYear {
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+    private  static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
